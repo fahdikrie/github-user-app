c7f4fe6f696e5df8446912abda136b9e63066433
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-29 06:51:40 +0700

    feat(settings): add integration with DataStore

--- app/src/main/java/com/dicoding/latihan/githubuser/activities/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/latihan/githubuser/activities/SettingsActivity.kt
@@ -1,10 +1,20 @@
 package com.dicoding.latihan.githubuser.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.latihan.githubuser.databinding.ActivitySettingsBinding
+import com.dicoding.latihan.githubuser.models.datastore.preferences.SettingPreference
+import com.dicoding.latihan.githubuser.viewmodels.SettingsViewModel
+import com.dicoding.latihan.githubuser.viewmodels.factories.SettingsViewModelFactory
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
     private var _binding: ActivitySettingsBinding? = null
@@ -19,14 +29,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun bindDarkThemeSwitch() {
+        val pref = SettingPreference.getInstance(dataStore)
+        val settingsViewModel = ViewModelProvider(this,
+            SettingsViewModelFactory(pref))[SettingsViewModel::class.java]
+
+        settingsViewModel.getThemeSettings().observe(this,
+            { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    binding.switchTheme.isChecked = true
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    binding.switchTheme.isChecked = false
+                }
+            })
+
         binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                binding.switchTheme.isChecked = true
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                binding.switchTheme.isChecked = false
-            }
+            settingsViewModel.saveThemeSetting(isChecked)
         }
     }
 }
---
+++ app/src/main/java/com/dicoding/latihan/githubuser/models/datastore/preferences/SettingsPreference.kt
@@ -0,0 +1,39 @@
+package com.dicoding.latihan.githubuser.models.datastore.preferences
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class SettingPreference private constructor(private val dataStore: DataStore<Preferences>) {
+
+    private val themeKey = booleanPreferencesKey("theme_setting")
+
+    fun getThemeSetting(): Flow<Boolean> {
+        return dataStore.data.map { preferences ->
+            preferences[themeKey] ?: false
+        }
+    }
+
+    suspend fun saveThemeSetting(isDarkModeActive: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[themeKey] = isDarkModeActive
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SettingPreference? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): SettingPreference {
+            return INSTANCE ?: synchronized(this) {
+                val instance = SettingPreference(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
