6ddf3552ce31c803e52aab4a6f88f2d49386f49c
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-29 17:48:48 +0700

    test: add tests

---
+++ app/src/androidTest/java/com/dicoding/latihan/githubuser/activities/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.dicoding.latihan.githubuser.activities
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.latihan.githubuser.R
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+    @Before
+    fun setup(){
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun assertSearchViewIsShowing() {
+        onView(withId(R.id.sv_search_user)).check(matches(isDisplayed()))
+    }
+}
